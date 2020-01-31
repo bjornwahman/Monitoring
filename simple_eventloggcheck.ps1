33bfa2c935beb8c2d1974a8be4e9d0b96a4ba1e4
bjornwahman <bjornerikzzon@gmail.com>
2020-01-31 15:05:27 +0100

    Update simple_eventloggcheck.ps1

--- simple_eventloggcheck.ps1
+++ simple_eventloggcheck.ps1
@@ -1,8 +1,16 @@
-﻿$event = Get-EventLog -LogName System -EntryType Error -Source Tcpip | Select-Object -Property EventID, Message
+param(
+[string]$log = "System",
+[string]$source = "Tcpip",
+[string]$message = "index 8"
+)
+$event = @()
+$event = Get-EventLog -LogName $log -EntryType Error -Source $source -Message *$message* | Select -ExpandProperty Message
+#$eventerror = $event | out-string
+$eventerror = [system.String]::Join(" , ", $event)
     if ($event -ne $null)
     {
     $statuscode=2
-    Write-Host "CRITICAL: there is errors in ya eventlogg man"
+    Write-Host "CRITICAL: there is errors in ya eventlogg man, $eventerror"
     }
     else 
     {
