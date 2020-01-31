3b95754cfe7da4247bf8a2533d3dbb509009f2ac
bjornwahman <bjornerikzzon@gmail.com>
2020-01-31 13:48:18 +0100

    Add files via upload

---
+++ service_test.ps1
@@ -0,0 +1,16 @@
+﻿$item = Get-Service -Name "AppReadiness" | Select-Object -Property Status
+
+#Write-Host $item
+
+$statuscode = 0
+$statusOutput = "OK: todeloo $item"
+
+$statusobject    
+    if($item.Status -ne "Running"){
+    $statuscode=2
+    $statusOutput = "Service error $item"
+}
+
+Write-Host $statusOutput
+
+exit $statuscode
