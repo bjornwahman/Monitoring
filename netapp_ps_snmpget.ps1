d28536c462a6bad994bb1a7079b05efff771f6d9
bjornwahman <bjornerikzzon@gmail.com>
2020-01-31 13:24:42 +0100

    Update netapp_ps_snmpget.ps1

--- netapp_ps_snmpget.ps1
+++ netapp_ps_snmpget.ps1
@@ -1,6 +1,6 @@
 ﻿#Install-Module -Name Proxx.SNMP 
-$NetappVolumes = Invoke-SnmpWalk -IpAddress 10.66.32.114 -Community "fR0yd1s" -Version Ver2 -Oid ".1.3.6.1.4.1.789.1.5.4.1.2"
-$NetappUtilization = Invoke-SnmpWalk -IpAddress 10.66.32.114 -Community "fR0yd1s" -Version Ver2 -Oid ".1.3.6.1.4.1.789.1.5.4.1.6"
+$NetappVolumes = Invoke-SnmpWalk -IpAddress 127.0.0.1 -Community "snmpcn" -Version Ver2 -Oid ".1.3.6.1.4.1.789.1.5.4.1.2"
+$NetappUtilization = Invoke-SnmpWalk -IpAddress 127.0.0.1 -Community "snmpcn" -Version Ver2 -Oid ".1.3.6.1.4.1.789.1.5.4.1.6"
 #Smash together Volume and Utilization data
 $MyVolumes=@()
 $IamHere=-1
