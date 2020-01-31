334edfa011c858507fb93784ad8a9022e00485d6
bjornwahman <bjornerikzzon@gmail.com>
2020-01-31 13:36:52 +0100

    Add files via upload

---
+++ get_send_port_groupsv2.ps1
@@ -0,0 +1,48 @@
+﻿###################
+##Björn Wahman 2019-11-12
+##
+##Checks for something called a "SendPortgroup" in an application named "Biztalk"... :)
+###################
+$Biztalk_server = "SQLSERVER\INSTANS"
+
+[void] [System.Reflection.Assembly]::loadwithPartialName("Microsoft.BizTalk.ExplorerOM");
+
+$BTSexp = New-Object Microsoft.BizTalk.ExplorerOM.BtsCatalogExplorer;
+
+$BTSexp.ConnectionString = `
+"Server=$Biztalk_server;Initial Catalog=BizTalkMgmtDb;Integrated Security=SSPI;";
+$y = 0; [Array] $MyNewObject = $null;
+$y = 0; [Array] $MyNewObject = $null;
+$myObjects=@()
+$MyNewObject = foreach($item in ($BTSexp.SendPortGroups))
+{
+$newPSitem = New-Object PSObject -Property @{
+seq = $y.ToString("000");
+Application = $item.Application.name;
+Status = $item.Status;
+Port = $item.Name;
+}
+$myObjects+=$newPSitem
+}
+$statuscode = 0
+$statusOutput = "OK: SendPortGroup $($item.name) is started"
+
+$myErrorObjects=@()
+foreach ($object in $myObjects){
+    if ($object.status -ne "started"){
+        $myErrorObjects+=$object
+    }
+}
+
+if ($myErrorObjects.count -gt 0){
+    $statuscode=2
+    $statusOutput = "CRITICAL Check these SendPortGroups: "
+    $myErrorObjects | ForEach-Object {
+        $statusOutput = $statusOutput + $_.port + ", "
+    }
+    $statusOutput = $statusOutput.TrimEnd(", ")
+}
+
+Write-Host $statusOutput
+
+exit $statuscode
