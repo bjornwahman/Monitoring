3f70d187232c21a935272da843b0245504ceed8f
bjornwahman <bjornerikzzon@gmail.com>
2020-01-31 14:04:09 +0100

    Update getfile_pattern.ps1

--- getfile_pattern.ps1
+++ getfile_pattern.ps1
@@ -1,14 +1,16 @@
-﻿# 2020-01-31 Björn Wahman
+# 2020-01-31 Björn Wahman
 # Letar efter strings i textfiler och ge statuskoder till opsview beroende på vad den hittar
 param(
 [string]$file = "C:\trove\log.txt",
-[string]$filefilter = "voltage"
+[string]$textstring = "voltage"
 )
-    $fileobjects = Select-String -Path $file -Pattern "$filefilter"
+
+$fileobjects = @()
+    $fileobjects = Select-String -Path $file -Pattern "$textstring"
     if ($fileobjects -ne $null)
     {
     $statuscode=2
-    Write-Host "CRITICAL: you are fucked, the string $filefilter has been found, do you have access to your prepping gear? underground bunker is feature complete?"
+    Write-Host "CRITICAL: you are fucked, the string $textstring has been found, do you have access to your prepping gear? underground bunker is feature complete?"
     }
     else 
     {
