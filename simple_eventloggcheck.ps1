22d48c2b416f06a2a3fd910e27b45d5ed3a27068
bjornwahman <bjornerikzzon@gmail.com>
2020-01-31 13:58:39 +0100

    Add files via upload

---
+++ simple_eventloggcheck.ps1
@@ -0,0 +1,13 @@
+﻿$event = Get-EventLog -LogName System -EntryType Error -Source Tcpip | Select-Object -Property EventID, Message
+    if ($event -ne $null)
+    {
+    $statuscode=2
+    Write-Host "CRITICAL: there is errors in ya eventlogg man"
+    }
+    else 
+    {
+    Write-Host "OK: all is well in monitoring town, grab a cofee and relax"
+    $statuscode=0
+    
+    }
+exit $statuscode 
