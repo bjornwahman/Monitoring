c3fb5cef4939ec4d3ecf777ea9f67b55fc77eabc
bjornwahman <bjornerikzzon@gmail.com>
2020-02-05 15:31:36 +0100

    Update check_snmp_cryptoserver.ps1

--- check_snmp_cryptoserver.ps1
+++ check_snmp_cryptoserver.ps1
@@ -1,15 +1,18 @@
+#Temperature on Cryptoserver
+#Bjorn Wahman 2020-02-05
 #Install-Module -Name Proxx.SNMP 
-$Temperature = Invoke-SnmpGet -IpAddress 1.1.1.1 -Community "public" -Version Ver2 -Oid ".1.3.6.1.4.1.3159.1.2.1.6"
+
+$Temperature = Invoke-SnmpGet -IpAddress 1.1.1.1 -Community "public" -Version Ver2 -Oid ".1.3.6.1.4.1.3159.1.2.1.6.1"
 $myoutput = $Temperature.Value
 
 #$myoutput = 95
 
-if ($myoutput -ge 98) {
+if ($myoutput -ge 60) {
     $statuscode=2
     Write-Host "CRITICAL: The temperature is $myoutput °C"
     }
     ElseIf
-    ($myoutput -ge 96) {
+    ($myoutput -ge 50) {
     $statuscode=2
     Write-Host "WARNING: The temperature is $myoutput °C"
     }
